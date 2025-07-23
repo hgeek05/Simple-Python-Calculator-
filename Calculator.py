b47f327a164b22e360a2c413d66b474a29a6118e
Hiba <hibabelmoumene05@gmail.com>
2025-07-23 20:45:01 +0100

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+def calculator():
+    print("Simple Calculator")
+    print("Operations: +, -, *, /")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    op = input("Enter operator (+, -, *, /): ")
+    
+    
+    if op == '+':
+        print("Result:", num1 + num2)
+    elif op == '-':
+        print("Result:", num1 - num2)
+    elif op == '*':
+        print("Result:", num1 * num2)
+    elif op == '/':
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Cannot divide by zero!")
+    else:
+        print("Invalid operator")
+
+calculator()
+
+
+
